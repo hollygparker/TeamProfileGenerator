972b2d46d3b2d4b2e5834dc18f665bdffde3b713
Holly Parker <hollygcochran@gmail.com>
2022-08-22 09:42:27 -0500

    getting email to mailTo

--- src/template.js
+++ src/template.js
@@ -12,7 +12,7 @@ const fileTemplate = responses => {
             <h1 class="card-title">${engineer.getRole()}</h1>
                 <ul class="list-group">
                     <li class="list-group-item">ID: ${engineer.getId()}</li>
-                    <li class="list-group-item">Email: ${engineer.getEmail()}</li>
+                    <li class="list-group-item">Email:<a href="mailto:${engineer.getEmail()}"> ${engineer.getEmail()}</a></li>
                     <li class="list-group-item">GitHub: ${engineer.getGithub()}</li>
                 </ul>
           </div>
@@ -29,7 +29,7 @@ const fileTemplate = responses => {
             <h1 class="card-title">${intern.getRole()}</h1>
                 <ul class="list-group">
                     <li class="list-group-item">ID: ${intern.getId()}</li>
-                    <li class="list-group-item">Email: ${intern.getEmail()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
                     <li class="list-group-item">School: ${intern.getSchool()}</li>
                 </ul>
           </div>
@@ -45,7 +45,7 @@ const fileTemplate = responses => {
             <h1 class="card-title">${manager.getRole()}</h1>
                 <ul class="list-group">
                     <li class="list-group-item">ID: ${manager.getId()}</li>
-                    <li class="list-group-item">Email: ${manager.getEmail()}</li>
+                    <li class="list-group-item">Email:<a href="mailto:${manager.getEmail()}"> ${manager.getEmail()}</a></li>
                     <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
                 </ul>
             </div>
